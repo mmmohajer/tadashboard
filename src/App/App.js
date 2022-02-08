363bb1539cfa560e6b363eee0bbc3a8bd0626de8
Davidhosseini <d.hoseini2009@gmail.com>
2022-02-08 15:52:20 -0700

    having some error with app.js, differents props need to be considered

--- src/App/App.js
+++ src/App/App.js
@@ -8,41 +8,48 @@ import AdminNav from "Components/AdminNav";
 import Charts from "Components/Charts";
 // import Datatable from "Components/Datatable";
 import Dashboard from "Components/Dashboard";
-function App() {
-  const [frame, setFrame] = useState("Dashboard");
 
-  // constructor(props)= {
-  //   super(props); 
-  //   this.state={
-  //     chartData:{
-         
-  //     }
-  //   };
-  // }
-  // getChartData(){
-  //   this.setState({ labels: ["Innitiated", "Not Started1", "Canceled", "Completed"],
-  //   datasets: {label:'Test',data:[12.21, 4, 11.25, 50]},
-  //  backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745']});
-  // }
+class App extends Component {
+  constructor(props)= {
+    super(props); 
+    this.state={
+      chartData:{},
+      frame:Dashboard
 
-  return (
-    <>
-      <div className="flex pos-rel">
-        {/* <Header />  */}
-        <div className={cx(styles.adminNavContainer)}>
-          
-          <AdminNav setFrame={setFrame} />
-          
-        </div>
-        <div className="w-per-100">
-          {frame === "Dashboard" && <Dashboard />}
-          {frame === "Charts" && <Charts />}
-          {/* chartData={this.state.chartData} */}
-        </div>
-      </div>
-      <div className="footer">Footer</div>
-    </>
-  );
+    };
+  }
+   getChartData(){
+    this.setState({ labels: ["Innitiated", "Not Started1", "Canceled", "Completed"],
+    datasets: {label:'Test',data:[12.21, 4, 11.25, 50]},
+   backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745']});
+  }
+
+  render () {
+    // function App() {
+    //   classconst [frame, setFrame] = useState("Dashboard");
+      return (
+        <>
+          <div className="flex pos-rel">
+            {/* <Header />  */}
+            <div className={cx(styles.adminNavContainer)}>
+              
+              {/* <AdminNav setFrame={setFrame} /> */}
+              <AdminNav setFrame={this.state.frame} />
+              
+            </div>
+            <div className="w-per-100">
+              {frame === "Dashboard" && <Dashboard />}
+              {frame === "Charts" && <Charts />}
+              {/* chartData={this.state.chartData} */}
+            </div>
+          </div>
+          <div className="footer">Footer</div>
+        </>
+      );
+  }
 }
+
+
+
 
 export default App;
